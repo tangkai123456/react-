cfe6be4edeb9558a2d110e610aee0d304def3126
os-tangkai <os-tangkai@jsfund.cn>
2018-04-13 17:27:14 +0800

    ReactBaseClasses

--- packages/react/src/ReactBaseClasses.js
+++ packages/react/src/ReactBaseClasses.js
@@ -18,6 +18,7 @@ function Component(props, context, updater) {
   this.props = props;
   this.context = context;
   this.refs = emptyObject;
+  // 我们初始化了一个默认的updater，但是真实的updater在渲染之后才挂载
   // We initialize the default updater but the real one gets injected by the
   // renderer.
   this.updater = updater || ReactNoopUpdateQueue;
@@ -26,17 +27,23 @@ function Component(props, context, updater) {
 Component.prototype.isReactComponent = {};
 
 /**
+ * 一个设置状态的方法。仅使用这个方法来改变状态。你应该视`this.state`为不可变的
  * Sets a subset of the state. Always use this to mutate
  * state. You should treat `this.state` as immutable.
  *
+ * 不保证`this.state`马上会更新，所以在调用该方法后使用`this.state`可能会得到旧值
  * There is no guarantee that `this.state` will be immediately updated, so
  * accessing `this.state` after calling this method may return the old value.
  *
+ * 无法保证`setState`会同步运行，因为它们最终可能会一起批处理。你可以传入一个回调方法，这个回调方法会在设置状态完成之后执行
  * There is no guarantee that calls to `setState` will run synchronously,
  * as they may eventually be batched together.  You can provide an optional
  * callback that will be executed when the call to setState is actually
  * completed.
  *
+ * 当一个方法被传入setState，它将在未来的某一时刻被调用(异步的)。
+ * 会使用最新的组件参数(state,props,context)来调用这个方法。
+ * 这些值可能与this中的值不同，因为方法可能在receiveProps和shouldComponentUpdate之间调用,此时这些属性还没有写入this上
  * When a function is provided to setState, it will be called at some point in
  * the future (not synchronously). It will be called with the up to date
  * component arguments (state, props, context). These values can be different
@@ -44,30 +51,35 @@ Component.prototype.isReactComponent = {};
  * shouldComponentUpdate, and this new state, props, and context will not yet be
  * assigned to this.
  *
- * @param {object|function} partialState Next partial state or function to
- *        produce next partial state to be merged with current state.
- * @param {?function} callback Called after state is updated.
+ * @param {object|function} partialState 
+ * 下一个局部state 或者 用来生成下一个局部state的方法 将与现在的state进行合并 
+ * Next partial state or function to
+ * produce next partial state to be merged with current state.
+ * @param {?function} callback state更新完成后调用的方法 Called after state is updated.
  * @final
  * @protected
  */
-Component.prototype.setState = function(partialState, callback) {
+Component.prototype.setState = function (partialState, callback) {
   invariant(
     typeof partialState === 'object' ||
-      typeof partialState === 'function' ||
-      partialState == null,
+    typeof partialState === 'function' ||
+    partialState == null,
     'setState(...): takes an object of state variables to update or a ' +
-      'function which returns an object of state variables.',
+    'function which returns an object of state variables.',
   );
   this.updater.enqueueSetState(this, partialState, callback, 'setState');
 };
 
 /**
+ * 强制一次更新。不在DOM事务中时，才能调用该方法。
  * Forces an update. This should only be invoked when it is known with
  * certainty that we are **not** in a DOM transaction.
  *
+ * 如果你知道组件更深层的组件state已经改变了，但是没有调用setState，就可以调用这个方法
  * You may want to call this when you know that some deeper aspect of the
  * component's state has changed but `setState` was not called.
  *
+ * 该方法不会触发`shouldComponentUpadte`
  * This will not invoke `shouldComponentUpdate`, but it will invoke
  * `componentWillUpdate` and `componentDidUpdate`.
  *
@@ -75,11 +87,13 @@ Component.prototype.setState = function(partialState, callback) {
  * @final
  * @protected
  */
-Component.prototype.forceUpdate = function(callback) {
+Component.prototype.forceUpdate = function (callback) {
   this.updater.enqueueForceUpdate(this, callback, 'forceUpdate');
 };
 
 /**
+ * 不赞成使用的API。这些API存在与传统的react类中，我们打算启用他们，我们不会将它们放在现在的类中。
+ * 相反，我们定义一个在使用时发出警告的getter
  * Deprecated APIs. These APIs used to exist on classic React classes but since
  * we would like to deprecate them, we're not going to move them over to this
  * modern base class. Instead, we define a getter that warns if it's accessed.
@@ -89,17 +103,17 @@ if (__DEV__) {
     isMounted: [
       'isMounted',
       'Instead, make sure to clean up subscriptions and pending requests in ' +
-        'componentWillUnmount to prevent memory leaks.',
+      'componentWillUnmount to prevent memory leaks.',
     ],
     replaceState: [
       'replaceState',
       'Refactor your code to use setState instead (see ' +
-        'https://github.com/facebook/react/issues/3236).',
+      'https://github.com/facebook/react/issues/3236).',
     ],
   };
-  const defineDeprecationWarning = function(methodName, info) {
+  const defineDeprecationWarning = function (methodName, info) {
     Object.defineProperty(Component.prototype, methodName, {
-      get: function() {
+      get: function () {
         lowPriorityWarning(
           false,
           '%s(...) is deprecated in plain JavaScript React classes. %s',
@@ -117,10 +131,11 @@ if (__DEV__) {
   }
 }
 
-function ComponentDummy() {}
+function ComponentDummy() { }
 ComponentDummy.prototype = Component.prototype;
 
 /**
+ * 在shouldComponentUpdate自动进行浅对比的方便使用的组件
  * Convenience component with default shallow equality check for sCU.
  */
 function PureComponent(props, context, updater) {
@@ -136,4 +151,4 @@ pureComponentPrototype.constructor = PureComponent;
 Object.assign(pureComponentPrototype, Component.prototype);
 pureComponentPrototype.isPureReactComponent = true;
 
-export {Component, PureComponent};
+export { Component, PureComponent };
